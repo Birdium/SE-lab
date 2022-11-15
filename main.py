35f2e0861584b0b531a65195fe3f43682ad13320
Birdium <bibishu2001@163.com>
2022-11-16 02:12:53 +0800

    main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+from inputter import Inputter
+
+input_path = "input"
+output_path = "output"
+
+
+def main():
+    inputter = Inputter(input_path)
+    # TBD
+
+
+if __name__ == "__main__":
+    main()
