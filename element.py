1fa569c86d27a177beb38d078257c882f57bb9c4
Birdium <bibishu2001@163.com>
2022-11-15 19:50:19 +0800

    element

---
+++ element.py
@@ -0,0 +1,16 @@
+class Element:
+
+    def get_type(self):
+        return self.__type__
+
+    def get_lower(self):
+        return self.__lower__
+
+    def get_upper(self):
+        return self.__upper__
+
+    def __init__(self, item : tuple):
+        self.__type__ = item[0]
+        if item[0] == "int" or item[0] == "string":
+            self.__lower__ = item[1]
+            self.__upper__ = item[2]
